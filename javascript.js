d0bd519e7ac1dc35c68722ad3a09e452b448b155
Salmane <88793063+salmaane@users.noreply.github.com>
2022-09-02 14:39:32 +0100

    intro section project

---
+++ javascript.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let feature_drop = document.getElementById('features');
+let company_drop = document.getElementById('company');
+
+function dropMenu(dropdown) {
+  if(dropdown.style.maxHeight !== "0px") {
+    dropdown.style.maxHeight = "0";
+  } else {
+    dropdown.style.maxHeight = "10rem";
+  }
+}
+
+
+let menu = document.getElementById('navigation');
+
+function openMenu(){
+  if(menu.style.display === 'none') {
+    menu.style.display = 'flex';
+  } else {
+    menu.style.display = 'none';
+  }
+}
+
